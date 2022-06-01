2e5da28c96efb918cae520757761cfda37ae484b
blueberrylem <67043521+blueberrylem@users.noreply.github.com>
2022-06-01 13:28:41 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,272 @@
+namespace SpriteKind {
+    export const Background = SpriteKind.create()
+}
+function Scary_sound () {
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(139, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(139, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.playTone(139, music.beat(BeatFraction.Quarter))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.playTone(139, music.beat(BeatFraction.Quarter))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(139, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(139, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.playTone(139, music.beat(BeatFraction.Quarter))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.playTone(139, music.beat(BeatFraction.Quarter))
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(784, music.beat(BeatFraction.Quarter))
+    music.playTone(831, music.beat(BeatFraction.Quarter))
+    music.playTone(784, music.beat(BeatFraction.Quarter))
+    music.playTone(831, music.beat(BeatFraction.Quarter))
+    music.playTone(784, music.beat(BeatFraction.Half))
+    music.playTone(831, music.beat(BeatFraction.Half))
+    music.playTone(784, music.beat(BeatFraction.Half))
+    music.playTone(831, music.beat(BeatFraction.Quarter))
+    music.playTone(784, music.beat(BeatFraction.Quarter))
+    music.playTone(831, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(220, music.beat(BeatFraction.Quarter))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Quarter))
+    music.playTone(932, music.beat(BeatFraction.Quarter))
+}
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    Rocket = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........77777777........
+        ........77777777........
+        ........77f17f17........
+        ........77777777........
+        ........77777777........
+        ........77777777........
+        ........77222277........
+        ........77777777........
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Player)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
+    game.over(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    YO = textsprite.create("YOU WON!!!")
+    tiles.setCurrentTilemap(tilemap`level4`)
+    tiles.placeOnRandomTile(Rocket, assets.tile`myTile11`)
+    for (let index = 0; index < 100; index++) {
+        Coin = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . 5 4 4 5 . . . . . . 
+            . . . . . 5 4 5 5 4 5 . . . . . 
+            . . . . . 5 4 5 5 4 5 . . . . . 
+            . . . . . 5 4 5 5 4 5 . . . . . 
+            . . . . . . 5 4 4 5 . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        tiles.placeOnRandomTile(Coin, assets.tile`myTile15`)
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.baDing.play()
+    Point += 1
+    otherSprite.destroy()
+})
+let Run_speed = 0
+let Point = 0
+let YO: TextSprite = null
+let Coin: Sprite = null
+let Rocket: Sprite = null
+let textSprite = textsprite.create("0")
+Rocket = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........77777777........
+    ........77777777........
+    ........77f17f17........
+    ........77777777........
+    ........77777777........
+    ........77777777........
+    ........77222277........
+    ........77777777........
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+Rocket.setPosition(5, 200)
+tiles.placeOnRandomTile(Rocket, assets.tile`myTile11`)
+scene.cameraFollowSprite(Rocket)
+for (let index = 0; index < 50; index++) {
+    Coin = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . . 5 4 4 5 . . . . . . 
+        . . . . . 5 4 5 5 4 5 . . . . . 
+        . . . . . 5 4 5 5 4 5 . . . . . 
+        . . . . . 5 4 5 5 4 5 . . . . . 
+        . . . . . . 5 4 4 5 . . . . . . 
+        . . . . . . . 5 5 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    tiles.placeOnRandomTile(Coin, assets.tile`myTile12`)
+}
+forever(function () {
+    Scary_sound()
+})
+forever(function () {
+    textSprite.setText(convertToText(Point))
+    textSprite.setPosition(scene.cameraProperty(CameraProperty.Left) + 30, scene.cameraProperty(CameraProperty.Top) + 50)
+})
+forever(function () {
+    if (controller.down.isPressed()) {
+    	
+    }
+})
+forever(function () {
+	
+})
+forever(function () {
+    tiles.placeOnRandomTile(Coin, assets.tile`myTile12`)
+})
+forever(function () {
+    if (controller.up.isPressed()) {
+        Rocket.y += 1
+    }
+    if (controller.down.isPressed()) {
+        Rocket = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........77777777........
+            ........77f17f17........
+            ........77777777........
+            ........77222277........
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Player)
+    }
+    if (controller.right.isPressed()) {
+        Rocket.x += 1
+    }
+    if (controller.left.isPressed()) {
+        Rocket.x += -1
+    }
+    Rocket.setVelocity(Rocket.vx, 100)
+    Run_speed = Run_speed * 0.41
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,200 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile8], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1900190005050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050502020202020205050505050505050505050505050505050202020202070703050505050505050505050505050505050202020207020101010505050505050505050505050502020202020702010505050505050505050505050502020202020202020701050505050505050505050505050505020202020202020701050505050505050505050505050502020202020202070201050505050505050505050505020202020202020202020105050505050505050505050505050202020207070702010505050505050505050505050505050502070702010101050202020202020202050505050505050505020101070202020202020202020202020205050505050505050505050107070707020207070202070202020205050505050505050505010101010202010102020107020202020505050505050505050505050505050505050505010702020202050505050505050505050502020202020202020201020202020505050505050202020202020202020204020202020707020205050505050502020202020202070707040707070701010202050505020202020202020707020102020202020201050505050202020202020202070201010205050505050505050505050506020202070707020102050505050505050505050505050505020202070101010505050505050505050505050505050505050707070101050505050505050505050505050505050505050501010101010505050505050505050505050505050505050505`, img`
+.........................
+.........................
+...................222222
+.................22......
+................2.....222
+.............22.....2....
+.........2222.....2......
+.........2.......2.......
+.......22.......2........
+....222.......2..........
+............2............
+........222222222222.....
+....222.............2....
+.......2.............22..
+........2222..22..2....2.
+...................2...22
+...........2222222222....
+......22222..............
+......2..............222.
+...222.......222.2222....
+.22.......22.............
+........2................
+....222..................
+...22....................
+22222....................
+`, [myTiles.transparency16,myTiles.tile8,myTiles.tile9,myTiles.tile4,myTiles.tile10,myTiles.tile11,myTiles.tile14,myTiles.tile15], TileScale.Sixteen);
+            case "level5":
+            case "level5":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`1000100004040404040404040404040401050507040404040404040404040404050404040404040404040404040404050404040404040404050405040504050404040404040405040404040404040404040404040504040404040404040404040404040404050404040404040404040404040404040405040504050405040504050404040404040404040404040404040404050404040404040404040404040404050404040404040404040404040404040404040404040404040404040404050101040404040404050505040504050401040404030404050202020402040204040404040505050202040404040404040404040402020202060606060606060606060606`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 2 2 2 
+. . . . . . . . . . . . 2 . . . 
+. . . . . . . . . . . 2 . . . . 
+. . . . 2 . 2 . 2 . 2 . . . . . 
+. . 2 . 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . . 
+. 2 . . . . . . . . . . . . . . 
+. . 2 . 2 . 2 . 2 . 2 . 2 . . . 
+. . . . . . . . . . . . . . 2 . 
+2 2 2 2 2 2 2 2 2 2 . . . 2 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . 2 . . . . 
+. . . . 2 2 2 . 2 . 2 2 . . . . 
+. . . 2 2 . . . . . . . . . . . 
+2 2 2 2 . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile15,myTiles.tile17,myTiles.tile14,myTiles.tile9,myTiles.tile18,myTiles.tile11,myTiles.tile19], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "Dirt block":
+            case "tile8":return tile8;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile6":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile11":return tile11;
+            case "myTile9":
+            case "tile12":return tile12;
+            case "myTile10":
+            case "tile13":return tile13;
+            case "myTile11":
+            case "tile14":return tile14;
+            case "myTile7":
+            case "tile10":return tile10;
+            case "myTile12":
+            case "tile15":return tile15;
+            case "myTile13":
+            case "tile16":return tile16;
+            case "myTile14":
+            case "tile17":return tile17;
+            case "myTile15":
+            case "tile18":return tile18;
+            case "myTile16":
+            case "tile19":return tile19;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
